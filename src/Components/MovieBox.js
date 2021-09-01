7581134eef53bf8b77221522194fe7d6991253f4
YuetaoZ <60456187+yuetaoZ@users.noreply.github.com>
2021-09-01 10:13:59 -0500

    Added MovieCard design.

--- src/Components/MovieBox.js
+++ src/Components/MovieBox.js
@@ -1,12 +1,19 @@
 import React from "react";
 import "./MovieBox.css";
 import MovieBoxPage from "./MovieBoxPage";
+import MovieCard from "./MovieCard";
 
 const MovieBox = () => {
   return (
     <div className="movie-box-container">
       <MovieBoxPage></MovieBoxPage>
-      <div>MovieArea</div>
+      <div className="movie-box-display-area">
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
+      </div>
     </div>
   );
 };
---
+++ src/Components/MovieCard.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./MovieCard.css";
+
+const MovieCard = () => {
+  return (
+    <div className="movie-card">
+      <div className="movie-card-media">Media</div>
+      <div className="movie-card-title">Title</div>
+      <div className="movie-card-action-container">
+        <div>ratring</div>
+        <div>favorite</div>
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard;
