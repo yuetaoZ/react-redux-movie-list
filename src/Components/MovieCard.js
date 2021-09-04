e690d551ef2bd55711a77db1169686846e7770f2
YuetaoZ <60456187+yuetaoZ@users.noreply.github.com>
2021-09-04 15:58:32 -0500

    Updated MovieCard display.

--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -1,17 +1,48 @@
 import React from "react";
-import "../App.css";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
-const MovieCard = () => {
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+});
+
+const ImgMediaCard = (props) => {
+  const classes = useStyles();
+
   return (
-    <div className="movie-card">
-      <div className="movie-card-media">Media</div>
-      <div className="movie-card-title">Title</div>
-      <div className="movie-card-action-container">
-        <div>ratring</div>
-        <div>favorite</div>
-      </div>
-    </div>
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt="Movie backdrop"
+          height="500"
+          image={`https://image.tmdb.org/t/p/w500${props.movie.poster_path}`}
+          title={props.movie.title}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {props.movie.title}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Star
+        </Button>
+        <Button size="small" color="primary">
+          Favorite
+        </Button>
+      </CardActions>
+    </Card>
   );
 };
 
-export default MovieCard;
+export default ImgMediaCard;
